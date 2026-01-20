2304978262e470e7cf07b01f82018bd93530aca3
nabin <nabinshrestha348@gmail.com>
2026-01-20 01:43:18 -0800

    Initial Commit

---
+++ epahichan/epahichanApp.swift
@@ -0,0 +1,17 @@
+//
+//  epahichanApp.swift
+//  epahichan
+//
+//  Created by Nabin Shrestha on 1/20/26.
+//
+
+import SwiftUI
+
+@main
+struct epahichanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
